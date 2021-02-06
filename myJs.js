7524e3fdac962a72aa7f6f48d3bb543568c8828a
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2021-02-06 19:12:21 +0400

    updated Home

--- myJs.js
+++ myJs.js
@@ -6,8 +6,9 @@ var TriangleTop = document.getElementById("extInfshower");
 var showContent = document.getElementById("showContent");
 var myHeader = document.getElementById("myHeader");
 
+
 function showInfo(variant){
-        extInfo.style.display = "block";
+    extInfo.style.display = "block";
     switch(variant) {
         case 1:
             showHome();
@@ -21,7 +22,7 @@ function showInfo(variant){
     }
 }
 
-function closeInfo(){
+function closeInfo() {
     extInfo.style.display = "none";
     Home.style.backgroundImage = "url('icons/Home.svg')";
     Portfolio.style.backgroundImage = "url('icons/Portfolio.svg')";
@@ -32,22 +33,21 @@ function showHome() {
     Home.style.backgroundImage = "url('icons/Home2.svg')";
     Portfolio.style.backgroundImage = "url('icons/Portfolio.svg')";
     Contact.style.backgroundImage = "url('icons/Contact.svg')";
-    TriangleTop.style.top = 20;
     showContent.src = "pages/general.html";
     myHeader.innerHTML = "მოგესალმებით";
-    extInfo.style.overflowY = "scroll";
+    TriangleTop.style.top = '20px';
 }
 function showPortfolio() {
     Home.style.backgroundImage = "url('icons/Home.svg')";
     Portfolio.style.backgroundImage = "url('icons/Portfolio2.svg')";
     Contact.style.backgroundImage = "url('icons/Contact.svg')";
-    TriangleTop.style.top = 170;
+    TriangleTop.style.top = '170px';
     myHeader.innerHTML = "პორტფოლიო";
 }
 function showContact() {
     Home.style.backgroundImage = "url('icons/Home.svg')";
     Portfolio.style.backgroundImage = "url('icons/Portfolio.svg')";
     Contact.style.backgroundImage = "url('icons/Contact2.svg')";
-    TriangleTop.style.top = 320;
+    TriangleTop.style.top = '320px';
     myHeader.innerHTML = "კონტაქტი";
 }
