fb913e02e8a10c80484108b70320160aac3f6510
Ioane <38262019+ioane-stacks@users.noreply.github.com>
2021-02-06 08:23:31 +0400

    Add files via upload

---
+++ myJs.js
@@ -0,0 +1,53 @@
+var extInfo = document.getElementById("extInfBox");
+var Home = document.getElementById("Home");
+var Portfolio = document.getElementById("Portfolio");
+var Contact = document.getElementById("Contact");
+var TriangleTop = document.getElementById("extInfshower");
+var showContent = document.getElementById("showContent");
+var myHeader = document.getElementById("myHeader");
+
+function showInfo(variant){
+        extInfo.style.display = "block";
+    switch(variant) {
+        case 1:
+            showHome();
+            break;
+        case 2:
+            showPortfolio();
+            break;
+        case 3:
+            showContact();
+            break;
+    }
+}
+
+function closeInfo(){
+    extInfo.style.display = "none";
+    Home.style.backgroundImage = "url('Home.svg')";
+    Portfolio.style.backgroundImage = "url('Portfolio.svg')";
+    Contact.style.backgroundImage = "url('Contact.svg')";
+}
+
+function showHome() {
+    Home.style.backgroundImage = "url('Home2.svg')";
+    Portfolio.style.backgroundImage = "url('Portfolio.svg')";
+    Contact.style.backgroundImage = "url('Contact.svg')";
+    TriangleTop.style.top = 20;
+    showContent.src = "general.html";
+    myHeader.innerHTML = "მოგესალმებით";
+    extInfo.style.overflowY = "scroll";
+}
+function showPortfolio() {
+    Home.style.backgroundImage = "url('Home.svg')";
+    Portfolio.style.backgroundImage = "url('Portfolio2.svg')";
+    Contact.style.backgroundImage = "url('Contact.svg')";
+    TriangleTop.style.top = 170;
+    myHeader.innerHTML = "პორტფოლიო";
+}
+function showContact() {
+    Home.style.backgroundImage = "url('Home.svg')";
+    Portfolio.style.backgroundImage = "url('Portfolio.svg')";
+    Contact.style.backgroundImage = "url('Contact2.svg')";
+    TriangleTop.style.top = 320;
+    myHeader.innerHTML = "კონტაქტი";
+}
